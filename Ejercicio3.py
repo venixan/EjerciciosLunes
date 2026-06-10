0a37a40815b93a1c1b7a70971dd2ba0c4e5f8dff
venixan <ve.bermudez@duocuc.cl>
2026-06-09 21:10:10 -0400

    Se empieza el menú y su desarrollo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,5 @@
+import time
+
 inventario = {
     "Laptop" : 10,
     "Mouse" : 25,
@@ -9,3 +11,16 @@ while True:
     print("1.- Mostrar productos disponibles.")
     print("2.- Realizar una venta.")
     print("3.- Salir.")
+    try:
+        opc = int(input("Seleccione una opción: "))
+    except ValueError:
+        print("ERROR!! debe ingresar un número entero válido")
+        time.sleep(1.5)    
+        continue
+    if opc == 1:
+        print("--- PRODUCTOS EN STOCK ---")
+        for producto, stock in inventario.items():
+            print(f"- {producto}: {stock} unidades.")
+        input("Presione para volver al menú...")    
+    if opc == 2:
+        print("")    
