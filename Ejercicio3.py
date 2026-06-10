3a9d6a72e726f3d8c2169d598f582efef54db5bb
venixan <ve.bermudez@duocuc.cl>
2026-06-09 21:00:56 -0400

    Se realiza el segundo ejercicio e inicio de menú :3

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+inventario = {
+    "Laptop" : 10,
+    "Mouse" : 25,
+    "Teclado" : 15
+}
+
+while True:
+    print("*** SISTEMA DE INVENTARIO TITAN ***")
+    print("1.- Mostrar productos disponibles.")
+    print("2.- Realizar una venta.")
+    print("3.- Salir.")
